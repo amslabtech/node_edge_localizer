d7aff0a70814fcd3d4f6ea9c9d6923c3898627e2
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-04-27 18:30:01 +0900

    close #10

--- include/node_edge_localizer/node_edge_particle.h
+++ include/node_edge_localizer/node_edge_particle.h
@@ -1,6 +1,8 @@
 #ifndef __NODE_EDGE_PARTICLE_H
 #define __NODE_EDGE_PARTICLE_H
 
+#include <ros/ros.h>
+
 #include "Eigen/Dense"
 #include "Eigen/Geometry"
 
@@ -8,6 +10,24 @@ class NodeEdgeParticle
 {
 public:
 	NodeEdgeParticle(void);
+
+	double get_distance_from_last_node(double, double);
+	double square(double);
+	void move(double, double);
+	void evaluate(double);
+
+	double x;
+	double y;
+	// edge orientation
+	double yaw;
+	int current_edge_index;
+	int last_edge_index;
+	double moved_distance;
+	double last_node_x;
+	double last_node_y;
+	double weight;
+	bool near_node_flag;
+
 private:
 };
 
--- src/node_edge_particle.cpp
+++ src/node_edge_particle.cpp
@@ -1,6 +1,39 @@
 #include "node_edge_localizer/node_edge_particle.h"
 
 NodeEdgeParticle::NodeEdgeParticle(void)
+	:x(0.0), y(0.0), yaw(0.0),
+	 current_edge_index(-1), last_edge_index(-1), 
+	 moved_distance(0.0),
+	 last_node_x(0.0), last_node_y(0.0),
+	 weight(0.0), near_node_flag(false)
 {
 
+}
+
+double NodeEdgeParticle::get_distance_from_last_node(double x, double y)
+{
+	return sqrt(square(last_node_x - x) + square(last_node_y - y));
+}
+
+double NodeEdgeParticle::square(double value)
+{
+	return value * value;
+}
+
+void NodeEdgeParticle::move(double distance, double direction)
+{
+	yaw = direction;
+	x += distance * cos(yaw);
+	y += distance * sin(yaw);
+	moved_distance += distance;
+}
+
+void NodeEdgeParticle::evaluate(double edge_orientation)
+{
+	double diff = fabs(yaw - edge_orientation);	
+	diff = atan2(sin(diff), cos(diff));
+	weight = (1 - diff / M_PI);
+	if(weight < 1e-5){
+		weight = 1e-5;
+	}
 }
