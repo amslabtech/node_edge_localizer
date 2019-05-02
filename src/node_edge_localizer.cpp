526ba2eff824fd5a39d62e36e178c0746a24263a
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-05-02 12:41:15 +0900

    update default param

--- src/node_edge_localizer.cpp
+++ src/node_edge_localizer.cpp
@@ -144,8 +144,8 @@ NodeEdgeLocalizer::NodeEdgeLocalizer(void)
 	private_nh.param("INIT_PROGRESS", INIT_PROGRESS, {0.0});
 	private_nh.param("INIT_YAW", INIT_YAW, {0.0});
 	private_nh.param("CURVATURE_THRESHOLD", CURVATURE_THRESHOLD, {0.01});
-	private_nh.param("POSE_NUM_PCA", POSE_NUM_PCA, {30});
-	private_nh.param("MIN_LINE_SIZE", MIN_LINE_SIZE, {30});
+	private_nh.param("POSE_NUM_PCA", POSE_NUM_PCA, {15});
+	private_nh.param("MIN_LINE_SIZE", MIN_LINE_SIZE, {15});
 	private_nh.param("MIN_LINE_LENGTH", MIN_LINE_LENGTH, {3.0});
 	private_nh.param("ENABLE_TF", ENABLE_TF, {false});
 	private_nh.param("USE_ORIENTATION_Z_AS_YAW", USE_ORIENTATION_Z_AS_YAW, {false});
