9d62a9aa70e1255eea381dbab88e762e61593a3f
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2020-06-28 23:14:42 +0900

    change test

--- test/src/localization_test.cpp
+++ test/src/localization_test.cpp
@@ -4,15 +4,14 @@
 #include "amsl_navigation_msgs/NodeEdgeMap.h"
 #include "node_edge_localizer/localizer.h"
 
-class OdomSubTest: public ::testing::Test
+class LocalizationTest: public ::testing::Test
 {
 public:
-    OdomSubTest(void)
+    LocalizationTest(void)
     {
         odom_pub_ = nh_.advertise<nav_msgs::Odometry>("odom", 1, true);
         init_pose_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 1, true);
-        map_pub_ = nh_.advertise<amsl_navigation_msgs::NodeEdgeMap>("node_edge_map/map", 1, true);
-        pose_sub_ = nh_.subscribe("estimated_pose", 1, &OdomSubTest::pose_callback, this, ros::TransportHints().reliable().tcpNoDelay(true));
+        pose_sub_ = nh_.subscribe("estimated_pose", 1, &LocalizationTest::pose_callback, this, ros::TransportHints().reliable().tcpNoDelay(true));
     }
 
     void pose_callback(const nav_msgs::OdometryConstPtr& msg)
@@ -25,6 +24,8 @@ public:
 protected:
     virtual void SetUp()
     {
+        // wait for localizer receives the map
+        ros::Duration(1.0).sleep();
         estimated_pose_updated_ = false;
         pose_ = nav_msgs::Odometry();
         count_ = 0;
@@ -32,9 +33,7 @@ protected:
         init_pose.header.frame_id = "map";
         init_pose.pose.pose.orientation = get_quaternion_msg_from_yaw(0.0);
         init_pose_pub_.publish(init_pose);
-        amsl_navigation_msgs::NodeEdgeMap map;
-        map.header.frame_id = "map";
-        map_pub_.publish(map);
+        ros::Duration(0.5).sleep();
     }
 
     geometry_msgs::Quaternion get_quaternion_msg_from_yaw(const double yaw)
@@ -47,16 +46,14 @@ protected:
     ros::NodeHandle nh_;
     ros::Publisher odom_pub_;
     ros::Publisher init_pose_pub_;
-    ros::Publisher map_pub_;
     ros::Subscriber pose_sub_;
     bool estimated_pose_updated_;
     unsigned int count_;
     nav_msgs::Odometry pose_;
 };
 
-TEST_F(OdomSubTest, StopCase)
+TEST_F(LocalizationTest, StopCase)
 {
-    ros::Duration(1.0).sleep();
     nav_msgs::Odometry odom;
     odom.child_frame_id = "base_link";
     odom.header.frame_id = "odom";
@@ -81,9 +78,8 @@ TEST_F(OdomSubTest, StopCase)
     ASSERT_LT(abs(angle_diff), 0.01);
 }
 
-TEST_F(OdomSubTest, LinearCase)
+TEST_F(LocalizationTest, LinearCase)
 {
-    ros::Duration(1.0).sleep();
     nav_msgs::Odometry odom;
     odom.child_frame_id = "base_link";
     odom.header.frame_id = "odom";
@@ -112,9 +108,8 @@ TEST_F(OdomSubTest, LinearCase)
     ASSERT_LT(abs(angle_diff), 0.01);
 }
 
-TEST_F(OdomSubTest, LeftCurveCase)
+TEST_F(LocalizationTest, LeftCurveCase)
 {
-    ros::Duration(1.0).sleep();
     nav_msgs::Odometry odom;
     odom.child_frame_id = "base_link";
     odom.header.frame_id = "odom";
@@ -147,9 +142,8 @@ TEST_F(OdomSubTest, LeftCurveCase)
     ASSERT_LT(abs(angle_diff), 0.01);
 }
 
-TEST_F(OdomSubTest, RightCurveCase)
+TEST_F(LocalizationTest, RightCurveCase)
 {
-    ros::Duration(1.0).sleep();
     nav_msgs::Odometry odom;
     odom.child_frame_id = "base_link";
     odom.header.frame_id = "odom";
